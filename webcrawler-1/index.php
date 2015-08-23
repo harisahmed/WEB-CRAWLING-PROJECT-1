b88a44cb2dff9782e6b13610cbaca180ae8222b1
harisahmed <Compaq@Compaq-PC>
2015-08-23 10:23:09 +0500

    commit changes by thevasper

--- webcrawler-1/index.php
+++ webcrawler-1/index.php
@@ -9,22 +9,36 @@
         <?php
             include __DIR__.'/qp/qp.php';
 
-            $initial_url = 'https://pro.beatport.com/genre/deep-house/12/tracks';
-            $content = file_get_contents($initial_url);
+            $pages = array('https://pro.beatport.com/genre/deep-house/12/tracks');
+            $done = array();
 
-            //load qp with content fetched and initialise from body tag
-            $qp = htmlqp($content,'body');
-            echo '<pre>';
-            if($qp->length>0){
-                //we have some data to parse.
-                $tracks = $qp->find('.track');
-                foreach($tracks as $track){
+            
 
-                    echo 'Track Found:'.$track->find('.buk-track-primary-title')->first()->text()."\r\n";
+            while ($pages) {
 
+                set_time_limit(0);
+
+                $link = array_shift($pages);
+                $done[] = $link;
+                $content = file_get_contents($link);
+
+                //load qp with content fetched and initialise from body tag
+                $htmlqp = htmlqp($content,'body');
+                echo '<pre>';
+                if($htmlqp->length>0){
+                    //we have some data to parse.
+                    $tracks = $htmlqp->find('.track');
+                    foreach($tracks as $track){
+                        echo 'Track Found:'.$track->find('.buk-track-primary-title')->first()->text()."\r\n";
+                    }
+                    $next_page = $htmlqp->find('.pag-next')->first();
+                    foreach ($next_page as $link) {
+                        $page = 'https://pro.beatport.com'.$link->attr('href');
+                        echo 'Parsing page:'.$page;
+                        $pages[] = $page;
+                    }
                 }
             }
-
         ?>
     </body>
 </html>
