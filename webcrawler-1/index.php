b54e45deef7ba95c9759eb22f513b96f15f4c804
harisahmed <Compaq@Compaq-PC>
2015-08-23 10:48:12 +0500

    got artist name and crawled data for number of pages we need

--- webcrawler-1/index.php
+++ webcrawler-1/index.php
@@ -10,11 +10,12 @@
 
         function getFile($url){
 
+
             $url_hash = md5($url);
             if(file_exists($url_hash)){
                 return file_get_contents($url_hash);
             }else{
-                $content = file_get_contents($link);
+                $content = file_get_contents($url);
                 file_put_contents($url_hash, $content);
                 return $content;
             }
@@ -26,7 +27,7 @@
             $pages = array('https://pro.beatport.com/genre/deep-house/12/tracks');
             $done = array();
 
-
+            echo '<pre>';
             $ipCount = 0;
             while ($pages) {
 
@@ -39,19 +40,19 @@
 
                 //load qp with content fetched and initialise from body tag
                 $htmlqp = @htmlqp($content,'body');
-                echo '<pre>';
+
                 if($htmlqp->length>0){
                     //we have some data to parse.
                     $tracks = $htmlqp->find('.track');
                     foreach($tracks as $track){
-                        echo 'Track Found:'.$track->find('.buk-track-primary-title')->first()->text()."\r\n";
+                        echo 'Track Found:'.$track->find('.buk-track-primary-title')->first()->text(). '(' . $track->find('.buk-track-artists > a')->first()->text() . ")\r\n";
+
                     }
-                    $next_page = $htmlqp->find('.pag-next')->first();
-                    foreach ($next_page as $link) {
-                        $page = 'https://pro.beatport.com'.$link->attr('href');
-                        echo 'Parsing page:'.$page;
-                        $pages[] = $page;
-                    }
+                    $next_page = $htmlqp->find('.pag-next');
+                    if($next_page->length>0){
+                        $pages = array('https://pro.beatport.com'.$next_page->first()->attr('href'));
+                    }else
+                        $pages = false;
                 }
 
                 $ipCount++;
