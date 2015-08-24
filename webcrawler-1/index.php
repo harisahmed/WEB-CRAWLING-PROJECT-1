e409eb1ea8addb2786a1f651908542fe3d2de7ed
Varper <Varper@Battlecruiser>
2015-08-25 02:43:47 +0300

    Getting artist id using Spotify API. Perhaps more strict "if" statement needed.

--- webcrawler-1/index.php
+++ webcrawler-1/index.php
@@ -24,7 +24,7 @@ if(!file_exists(__DIR__.'/beatprot.sqlite')){
     <body>
         <h1>Web Crawler Project - 1</h1>
         <?php
-
+        
         function getFile($url){
 
 
@@ -40,7 +40,11 @@ if(!file_exists(__DIR__.'/beatprot.sqlite')){
         }
 
             include __DIR__.'/qp/qp.php';
-
+            require '/spotify-web-api-php-master/src/SpotifyWebAPI.php';
+            require '/spotify-web-api-php-master/src/Request.php';
+            
+            $api = new SpotifyWebAPI\SpotifyWebAPI();
+            
             $pages = array('https://pro.beatport.com/genre/deep-house/12/tracks');
             $done = array();
 
@@ -64,18 +68,44 @@ if(!file_exists(__DIR__.'/beatprot.sqlite')){
                     //we have some data to parse.
                     $tracks = $htmlqp->find('.track');
                     foreach($tracks as $track){
-
+                        
                         $title = $track->find('.buk-track-primary-title')->first()->text();
                         $artist = $track->find('.buk-track-artists > a')->first()->text();
                         $link_to_track = 'https://pro.beatport.com'.$track->find('.buk-track-title > a')->first()->attr('href');
-
+                        
+                        $spotify_artist = $api->search($artist, 'artist');
+                        
+                        //Geting artist id via Spotify Api
+                        
+                        foreach ($spotify_artist->artists->items as $spotify_id) {
+                            
+                            if (strpos($spotify_id->name,$artist) !== false) {
+                            
+                            echo '<br>';
+                            echo 'Beatport name is: '.$artist, '<br>';
+                            echo '<br>';
+                            echo 'Spotify name is: '.$spotify_id->name, '<br>';
+                            echo '<br>';
+                            echo 'Artist id on spotify is: '.$spotify_id->id, '<br>';
+                            
+                            }
+                            else {
+                                echo '<br>';
+                                echo 'Not matched!', '<br>';
+                                echo 'Beatport name is: '.$artist, '<br>';
+                                echo 'Spotify name is: '.$spotify_id->name, '<br>';
+                                
+                            }
+}
+                        
+                        
                         $id = $db->querySingle('select id from beatprottracks where track_name="'.$title.'" and artist="'.$artist.'"');
                         if($id){
                            $db->exec('update beatprottracks set link="'.$link_to_track.'" where id='.$id);
                         }else{
                             $db->exec('insert into beatprottracks ("track_name","artist","link") values ("'.$title.'","'.$artist.'","'.$link.'")');
                         }
-                    }
+                        }
                     $next_page = $htmlqp->find('.pag-next');
                     if($next_page->length>0){
                         $pages = array('https://pro.beatport.com'.$next_page->first()->attr('href'));
